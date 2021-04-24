006b898d95b226602b3176bf04c0cc97557fea07
KqkaricK <andrew73-42@yandex.ru>
2021-04-24 21:51:25 +0700

    Добавьте файлы проекта.

---
+++ VitchMat_Metodi_4/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿
+namespace VitchMat_Metodi_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.darkLabel1 = new DarkUI.Controls.DarkLabel();
+            this.t_d1 = new DarkUI.Controls.DarkTextBox();
+            this.darkLabel2 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel3 = new DarkUI.Controls.DarkLabel();
+            this.t_d2 = new DarkUI.Controls.DarkTextBox();
+            this.t_d4 = new DarkUI.Controls.DarkTextBox();
+            this.darkLabel5 = new DarkUI.Controls.DarkLabel();
+            this.t_d3 = new DarkUI.Controls.DarkTextBox();
+            this.darkLabel6 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel4 = new DarkUI.Controls.DarkLabel();
+            this.t_e = new DarkUI.Controls.DarkTextBox();
+            this.rb_pol = new DarkUI.Controls.DarkRadioButton();
+            this.rb_hord = new DarkUI.Controls.DarkRadioButton();
+            this.rb_kas = new DarkUI.Controls.DarkRadioButton();
+            this.rb_komb = new DarkUI.Controls.DarkRadioButton();
+            this.darkButton1 = new DarkUI.Controls.DarkButton();
+            this.t_n = new DarkUI.Controls.DarkLabel();
+            this.t_x = new DarkUI.Controls.DarkLabel();
+            this.t_ab1 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel7 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel8 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel9 = new DarkUI.Controls.DarkLabel();
+            this.t_ab2 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel11 = new DarkUI.Controls.DarkLabel();
+            this.darkButton2 = new DarkUI.Controls.DarkButton();
+            this.SuspendLayout();
+            // 
+            // darkLabel1
+            // 
+            this.darkLabel1.AutoSize = true;
+            this.darkLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel1.Location = new System.Drawing.Point(28, 23);
+            this.darkLabel1.Name = "darkLabel1";
+            this.darkLabel1.Size = new System.Drawing.Size(184, 13);
+            this.darkLabel1.TabIndex = 0;
+            this.darkLabel1.Text = "Введите числовые коэффициенты:";
+            // 
+            // t_d1
+            // 
+            this.t_d1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.t_d1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.t_d1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_d1.Location = new System.Drawing.Point(218, 21);
+            this.t_d1.Name = "t_d1";
+            this.t_d1.Size = new System.Drawing.Size(20, 20);
+            this.t_d1.TabIndex = 1;
+            this.t_d1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.darkTextBox5_KeyPress);
+            // 
+            // darkLabel2
+            // 
+            this.darkLabel2.AutoSize = true;
+            this.darkLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel2.Location = new System.Drawing.Point(238, 23);
+            this.darkLabel2.Name = "darkLabel2";
+            this.darkLabel2.Size = new System.Drawing.Size(36, 13);
+            this.darkLabel2.TabIndex = 2;
+            this.darkLabel2.Text = "x^3 + ";
+            // 
+            // darkLabel3
+            // 
+            this.darkLabel3.AutoSize = true;
+            this.darkLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel3.Location = new System.Drawing.Point(290, 23);
+            this.darkLabel3.Name = "darkLabel3";
+            this.darkLabel3.Size = new System.Drawing.Size(36, 13);
+            this.darkLabel3.TabIndex = 4;
+            this.darkLabel3.Text = "x^2 + ";
+            // 
+            // t_d2
+            // 
+            this.t_d2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.t_d2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.t_d2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_d2.Location = new System.Drawing.Point(270, 21);
+            this.t_d2.Name = "t_d2";
+            this.t_d2.Size = new System.Drawing.Size(20, 20);
+            this.t_d2.TabIndex = 3;
+            this.t_d2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.darkTextBox5_KeyPress);
+            // 
+            // t_d4
+            // 
+            this.t_d4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.t_d4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.t_d4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_d4.Location = new System.Drawing.Point(360, 21);
+            this.t_d4.Name = "t_d4";
+            this.t_d4.Size = new System.Drawing.Size(20, 20);
+            this.t_d4.TabIndex = 7;
+            this.t_d4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.darkTextBox5_KeyPress);
+            // 
+            // darkLabel5
+            // 
+            this.darkLabel5.AutoSize = true;
+            this.darkLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel5.Location = new System.Drawing.Point(340, 23);
+            this.darkLabel5.Name = "darkLabel5";
+            this.darkLabel5.Size = new System.Drawing.Size(24, 13);
+            this.darkLabel5.TabIndex = 6;
+            this.darkLabel5.Text = "x + ";
+            // 
+            // t_d3
+            // 
+            this.t_d3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.t_d3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.t_d3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_d3.Location = new System.Drawing.Point(320, 21);
+            this.t_d3.Name = "t_d3";
+            this.t_d3.Size = new System.Drawing.Size(20, 20);
+            this.t_d3.TabIndex = 5;
+            this.t_d3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.darkTextBox5_KeyPress);
+            // 
+            // darkLabel6
+            // 
+            this.darkLabel6.AutoSize = true;
+            this.darkLabel6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel6.Location = new System.Drawing.Point(168, 54);
+            this.darkLabel6.Name = "darkLabel6";
+            this.darkLabel6.Size = new System.Drawing.Size(16, 13);
+            this.darkLabel6.TabIndex = 20;
+            this.darkLabel6.Text = "0,";
+            // 
+            // darkLabel4
+            // 
+            this.darkLabel4.AutoSize = true;
+            this.darkLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel4.Location = new System.Drawing.Point(28, 54);
+            this.darkLabel4.Name = "darkLabel4";
+            this.darkLabel4.Size = new System.Drawing.Size(130, 13);
+            this.darkLabel4.TabIndex = 19;
+            this.darkLabel4.Text = "Введите погрешность е:";
+            // 
+            // t_e
+            // 
+            this.t_e.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.t_e.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.t_e.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_e.Location = new System.Drawing.Point(184, 52);
+            this.t_e.Name = "t_e";
+            this.t_e.Size = new System.Drawing.Size(100, 20);
+            this.t_e.TabIndex = 18;
+            this.t_e.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.darkTextBox5_KeyPress);
+            // 
+            // rb_pol
+            // 
+            this.rb_pol.AutoSize = true;
+            this.rb_pol.Checked = true;
+            this.rb_pol.Location = new System.Drawing.Point(31, 95);
+            this.rb_pol.Name = "rb_pol";
+            this.rb_pol.Size = new System.Drawing.Size(164, 17);
+            this.rb_pol.TabIndex = 21;
+            this.rb_pol.TabStop = true;
+            this.rb_pol.Text = "Метод половиного деления";
+            this.rb_pol.CheckedChanged += new System.EventHandler(this.rb_pol_CheckedChanged);
+            // 
+            // rb_hord
+            // 
+            this.rb_hord.AutoSize = true;
+            this.rb_hord.Location = new System.Drawing.Point(31, 118);
+            this.rb_hord.Name = "rb_hord";
+            this.rb_hord.Size = new System.Drawing.Size(83, 17);
+            this.rb_hord.TabIndex = 22;
+            this.rb_hord.Text = "Метод хорд";
+            this.rb_hord.CheckedChanged += new System.EventHandler(this.rb_pol_CheckedChanged);
+            // 
+            // rb_kas
+            // 
+            this.rb_kas.AutoSize = true;
+            this.rb_kas.Location = new System.Drawing.Point(31, 141);
+            this.rb_kas.Name = "rb_kas";
+            this.rb_kas.Size = new System.Drawing.Size(126, 17);
+            this.rb_kas.TabIndex = 23;
+            this.rb_kas.Text = "Метод касательных";
+            this.rb_kas.CheckedChanged += new System.EventHandler(this.rb_pol_CheckedChanged);
+            // 
+            // rb_komb
+            // 
+            this.rb_komb.AutoSize = true;
+            this.rb_komb.Location = new System.Drawing.Point(31, 164);
+            this.rb_komb.Name = "rb_komb";
+            this.rb_komb.Size = new System.Drawing.Size(154, 17);
+            this.rb_komb.TabIndex = 24;
+            this.rb_komb.Text = "Комбинированный метод";
+            this.rb_komb.CheckedChanged += new System.EventHandler(this.rb_pol_CheckedChanged);
+            // 
+            // darkButton1
+            // 
+            this.darkButton1.Location = new System.Drawing.Point(31, 213);
+            this.darkButton1.Name = "darkButton1";
+            this.darkButton1.Padding = new System.Windows.Forms.Padding(5);
+            this.darkButton1.Size = new System.Drawing.Size(118, 23);
+            this.darkButton1.TabIndex = 25;
+            this.darkButton1.Text = "Поехали";
+            this.darkButton1.Click += new System.EventHandler(this.darkButton1_Click);
+            // 
+            // t_n
+            // 
+            this.t_n.AutoSize = true;
+            this.t_n.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_n.Location = new System.Drawing.Point(323, 147);
+            this.t_n.Name = "t_n";
+            this.t_n.Size = new System.Drawing.Size(13, 13);
+            this.t_n.TabIndex = 31;
+            this.t_n.Text = "0";
+            // 
+            // t_x
+            // 
+            this.t_x.AutoSize = true;
+            this.t_x.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_x.Location = new System.Drawing.Point(323, 122);
+            this.t_x.Name = "t_x";
+            this.t_x.Size = new System.Drawing.Size(13, 13);
+            this.t_x.TabIndex = 30;
+            this.t_x.Text = "0";
+            // 
+            // t_ab1
+            // 
+            this.t_ab1.AutoSize = true;
+            this.t_ab1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_ab1.Location = new System.Drawing.Point(323, 98);
+            this.t_ab1.Name = "t_ab1";
+            this.t_ab1.Size = new System.Drawing.Size(13, 13);
+            this.t_ab1.TabIndex = 29;
+            this.t_ab1.Text = "0";
+            // 
+            // darkLabel7
+            // 
+            this.darkLabel7.AutoSize = true;
+            this.darkLabel7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel7.Location = new System.Drawing.Point(215, 147);
+            this.darkLabel7.Name = "darkLabel7";
+            this.darkLabel7.Size = new System.Drawing.Size(92, 13);
+            this.darkLabel7.TabIndex = 28;
+            this.darkLabel7.Text = "Число итераций:";
+            // 
+            // darkLabel8
+            // 
+            this.darkLabel8.AutoSize = true;
+            this.darkLabel8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel8.Location = new System.Drawing.Point(215, 122);
+            this.darkLabel8.Name = "darkLabel8";
+            this.darkLabel8.Size = new System.Drawing.Size(91, 13);
+            this.darkLabel8.TabIndex = 27;
+            this.darkLabel8.Text = "Значение корня:";
+            // 
+            // darkLabel9
+            // 
+            this.darkLabel9.AutoSize = true;
+            this.darkLabel9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel9.Location = new System.Drawing.Point(215, 98);
+            this.darkLabel9.Name = "darkLabel9";
+            this.darkLabel9.Size = new System.Drawing.Size(59, 13);
+            this.darkLabel9.TabIndex = 26;
+            this.darkLabel9.Text = "Интервал:";
+            // 
+            // t_ab2
+            // 
+            this.t_ab2.AutoSize = true;
+            this.t_ab2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_ab2.Location = new System.Drawing.Point(342, 98);
+            this.t_ab2.Name = "t_ab2";
+            this.t_ab2.Size = new System.Drawing.Size(13, 13);
+            this.t_ab2.TabIndex = 33;
+            this.t_ab2.Text = "0";
+            // 
+            // darkLabel11
+            // 
+            this.darkLabel11.AutoSize = true;
+            this.darkLabel11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel11.Location = new System.Drawing.Point(334, 97);
+            this.darkLabel11.Name = "darkLabel11";
+            this.darkLabel11.Size = new System.Drawing.Size(10, 13);
+            this.darkLabel11.TabIndex = 34;
+            this.darkLabel11.Text = ";";
+            // 
+            // darkButton2
+            // 
+            this.darkButton2.Location = new System.Drawing.Point(189, 213);
+            this.darkButton2.Name = "darkButton2";
+            this.darkButton2.Padding = new System.Windows.Forms.Padding(5);
+            this.darkButton2.Size = new System.Drawing.Size(118, 23);
+            this.darkButton2.TabIndex = 35;
+            this.darkButton2.Text = "След. Корень";
+            this.darkButton2.Visible = false;
+            this.darkButton2.Click += new System.EventHandler(this.darkButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 273);
+            this.Controls.Add(this.darkButton2);
+            this.Controls.Add(this.darkLabel11);
+            this.Controls.Add(this.t_ab2);
+            this.Controls.Add(this.t_n);
+            this.Controls.Add(this.t_x);
+            this.Controls.Add(this.t_ab1);
+            this.Controls.Add(this.darkLabel7);
+            this.Controls.Add(this.darkLabel8);
+            this.Controls.Add(this.darkLabel9);
+            this.Controls.Add(this.darkButton1);
+            this.Controls.Add(this.rb_komb);
+            this.Controls.Add(this.rb_kas);
+            this.Controls.Add(this.rb_hord);
+            this.Controls.Add(this.rb_pol);
+            this.Controls.Add(this.darkLabel6);
+            this.Controls.Add(this.darkLabel4);
+            this.Controls.Add(this.t_e);
+            this.Controls.Add(this.t_d4);
+            this.Controls.Add(this.darkLabel5);
+            this.Controls.Add(this.t_d3);
+            this.Controls.Add(this.darkLabel3);
+            this.Controls.Add(this.t_d2);
+            this.Controls.Add(this.darkLabel2);
+            this.Controls.Add(this.t_d1);
+            this.Controls.Add(this.darkLabel1);
+            this.Name = "Form1";
+            this.Text = "Вычисление корня";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DarkUI.Controls.DarkLabel darkLabel1;
+        private DarkUI.Controls.DarkTextBox t_d1;
+        private DarkUI.Controls.DarkLabel darkLabel2;
+        private DarkUI.Controls.DarkLabel darkLabel3;
+        private DarkUI.Controls.DarkTextBox t_d2;
+        private DarkUI.Controls.DarkTextBox t_d4;
+        private DarkUI.Controls.DarkLabel darkLabel5;
+        private DarkUI.Controls.DarkTextBox t_d3;
+        private DarkUI.Controls.DarkLabel darkLabel6;
+        private DarkUI.Controls.DarkLabel darkLabel4;
+        private DarkUI.Controls.DarkTextBox t_e;
+        private DarkUI.Controls.DarkRadioButton rb_pol;
+        private DarkUI.Controls.DarkRadioButton rb_hord;
+        private DarkUI.Controls.DarkRadioButton rb_kas;
+        private DarkUI.Controls.DarkRadioButton rb_komb;
+        private DarkUI.Controls.DarkButton darkButton1;
+        private DarkUI.Controls.DarkLabel t_n;
+        private DarkUI.Controls.DarkLabel t_x;
+        private DarkUI.Controls.DarkLabel t_ab1;
+        private DarkUI.Controls.DarkLabel darkLabel7;
+        private DarkUI.Controls.DarkLabel darkLabel8;
+        private DarkUI.Controls.DarkLabel darkLabel9;
+        private DarkUI.Controls.DarkLabel t_ab2;
+        private DarkUI.Controls.DarkLabel darkLabel11;
+        private DarkUI.Controls.DarkButton darkButton2;
+    }
+}
+
---
+++ VitchMat_Metodi_4/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarkUI.Forms;
+
+namespace VitchMat_Metodi_4
+{
+    public partial class Form1 : DarkForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void darkTextBox5_KeyPress(object sender, KeyPressEventArgs e) //запрет на ввод
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != 45 && number != 44) 
+            {
+                e.Handled = true;
+            }
+        }
+        int NatchZnachLeft = -100;
+        private void darkButton1_Click(object sender, EventArgs e) //кнопка запуска
+        {
+            if (chek() == false)
+            {
+                MessageBox.Show("Ошибка!");
+            }
+            else
+            {
+                NatchZnachLeft = -100;
+                if (otbor(NatchZnachLeft) == false)
+                {
+                    MessageBox.Show("Корней нет!");
+                    NatchZnachLeft = -100;
+                }
+                else if (rb_pol.Checked == true)
+                {
+                    metod_pol();
+                }
+                else if (rb_hord.Checked == true)
+                {
+                    metod_hord();
+                }
+                else if (rb_kas.Checked == true)
+                {
+                    metod_kas();
+                }
+                else if (rb_komb.Checked == true)
+                {
+                    metod_komb();
+                }
+            }
+            darkButton2.Visible = true;
+        }
+        void metod_pol() // метод пол дел
+        {
+            double E = Convert.ToDouble("0," + t_e.Text);
+            int n = 0;
+            double a, b, c;
+            a = Convert.ToDouble(t_ab1.Text);
+            b = Convert.ToDouble(t_ab2.Text);
+            while ((Math.Abs(b - a)) / 2 > E)
+            {
+                c = (a + b) / 2;
+                if (yr(c) * yr(a) < 0)
+                {
+                    b = c;
+                }
+                else if (yr(c) * yr(b) < 0)
+                {
+                    a = c;
+                }
+                n++;
+            }
+            t_x.Text = Convert.ToString((a + b) / 2);
+            t_n.Text = Convert.ToString(n);
+        }
+        void metod_hord() // метод хорд
+        {
+            double E = Convert.ToDouble("0," + t_e.Text);
+            int n = 1;
+            double a, b, xn;
+            a = Convert.ToDouble(t_ab1.Text);
+            b = Convert.ToDouble(t_ab2.Text);
+            xn = b - yr(b) * (b - a) / (yr(b) - yr(a));
+            while (Math.Abs(xn - b) > E)
+            {
+                b = xn;
+                xn = b - yr(b) * (b - a) / (yr(b) - yr(a));
+                n++;
+            }
+            t_x.Text = Convert.ToString(xn);
+            t_n.Text = Convert.ToString(n);
+        }
+        void metod_kas() //метод кас
+        {
+            double E = Convert.ToDouble("0," + t_e.Text);
+            int n = 1;
+            double a, xn;
+            a = Convert.ToDouble(t_ab1.Text);
+            xn = a - (yr(a) / yr_proizvod(a));
+            while (Math.Abs(xn - a) > E)
+            {
+                a = xn;
+                xn = a - (yr(a)/yr_proizvod(a));
+                n++;
+            }
+            t_x.Text = Convert.ToString(xn);
+            t_n.Text = Convert.ToString(n);
+        }
+        void metod_komb() //метод комб
+        {
+            double E = Convert.ToDouble("0," + t_e.Text);
+            int n = 1;
+            double a, b, an, bn;
+            a = Convert.ToDouble(t_ab1.Text);
+            b = Convert.ToDouble(t_ab2.Text);
+            an = a - (yr(a) / yr_proizvod(a));
+            bn = b - yr(b) * (b - a) / (yr(b) - yr(a));
+            while ((Math.Abs(bn - an)) / 2 > E)
+            {
+                a = an;
+                b = bn;
+                an = a - (yr(a) / yr_proizvod(a));
+                bn = b - yr(b) * (b - a) / (yr(b) - yr(a));
+                n++;
+            }
+            t_x.Text = Convert.ToString((bn + an) / 2);
+            t_n.Text = Convert.ToString(n);
+        }
+        double yr(double x) //уравнение 
+        {
+            return Convert.ToDouble(t_d1.Text) * (x * x * x) + Convert.ToDouble(t_d2.Text) * (x * x) + Convert.ToDouble(t_d3.Text) * x + Convert.ToDouble(t_d4.Text);
+        }
+        double yr_proizvod(double x) // производная
+        {
+            return 3 * Convert.ToDouble(t_d1.Text) * (x * x) + 2 * Convert.ToDouble(t_d2.Text) * x + Convert.ToDouble(t_d3.Text);
+        }
+        bool otbor(int j) //нахождение интервала
+        {
+            int h = 1;
+            bool error = true;
+            for (int i = j; i <= 100; i += h)
+            {
+                if (yr(i) == 0)
+                {
+                    error = false;
+                    break;
+                }
+                if (yr(i) * yr(i + h) < 0)
+                {
+                    t_ab1.Text = Convert.ToString(i);
+                    t_ab2.Text = Convert.ToString(i + h);
+                    NatchZnachLeft = i + h;
+                    error = false;
+                    break;
+                }
+            }
+            if (error == true)
+            {
+                return false;
+            }
+            return true;
+        }
+        bool chek() // проверка на правильный ввод
+        {
+            double d;
+            if (Double.TryParse(t_e.Text, out d) == false)
+            {
+                return false;
+            }
+            else if (Double.TryParse(t_d1.Text, out d) == false || Double.TryParse(t_d2.Text, out d) == false || Double.TryParse(t_d3.Text, out d) == false || Double.TryParse(t_d4.Text, out d) == false )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void darkButton2_Click(object sender, EventArgs e) // кнопка след корня
+        {
+            if (otbor(NatchZnachLeft) == false)
+            {
+                MessageBox.Show("Корней больше нет!");
+                darkButton2.Visible = false;
+                NatchZnachLeft = -100;
+            }
+            else if (rb_pol.Checked == true)
+            {
+                metod_pol();
+            }
+            else if (rb_hord.Checked == true)
+            {
+                metod_hord();
+            }
+            else if (rb_kas.Checked == true)
+            {
+                metod_kas();
+            }
+            else if (rb_komb.Checked == true)
+            {
+                metod_komb();
+            }
+        }
+
+        private void rb_pol_CheckedChanged(object sender, EventArgs e) 
+        {
+            darkButton2.Visible = false;
+            NatchZnachLeft = -100;
+        }
+    }
+}
